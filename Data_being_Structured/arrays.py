a3c2178a8f5f6ed509b29413bbf031673da7de3b
Monish Nule <imonishnule8@gmail.com>
2024-06-01 11:36:40 +0530

    added

---
+++ Data_being_Structured/arrays.py
@@ -0,0 +1,13 @@
+new_list = [1,2,3]
+# in py the array stores the references to the objets or values and value or actual objects is being stored in distinct 
+#places to each other in memory location.
+
+result = new_list[0]
+
+
+for n in new_list:
+    if n == 1:
+        print(True) 
+        
+        break
+
--- algos/binary_search.py
+++ algos/binary_search.py
@@ -13,13 +13,3 @@ def binary_search(list, target):
             last  =  midpoint -1
     return None
 
-def verify(index):
-    if index is not None:
-        print("Target found at the index:",index)
-    else:
-        print("target not found in the list")
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-
-result = binary_search(numbers, 6)
-verify(result)
